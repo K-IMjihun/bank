d84cb9964fe3823b6d654ba0d934d553f76dfa4c
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2022-12-21 18:39:22 +0900

    Main : 읽기쉽게 리팩토링

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-  UserSelectAndConnect userSelectAndConnect = new UserSelectAndConnect();
-  static boolean logInCheck = false;
+  static String select = "";
   public static void main(String[] args) {
-    while(true) {
+    while(!select.equals("9")) {
       Scanner sc = new Scanner(System.in);
-      System.out.println("XX은행입니다. 원하시는 번호를 입력해주세요");
-      System.out.println("1. 회원가입");
-      System.out.println("2. 아이디 / 비밀번호 찾기");
-      System.out.println("3. 로그인");
-      System.out.println("4. 로그아웃");
-      System.out.println("5. 입금");
-      System.out.println("6. 출금");
-      System.out.println("7. 잔액확인");
-      System.out.println("8. 종료");
-      String userSelect = sc.next();
-      UserSelectAndConnect.userSelect(userSelect);
-      if(userSelect.equals("8")){
-        break;
-      }
+          System.out.println(
+              "XX은행입니다. 원하시는 항목을 선택해주세요\n" +
+              "1. 회원가입\n" +
+              "2. 아이디 / 비밀번호 찾기\n" +
+              "3. 로그인 \n" +
+              "4. 로그아웃 \n" +
+              "5. 입금\n" +
+              "6. 송금\n" +
+              "7. 출금\n" +
+              "8. 잔액확인\n" +
+              "9. 종료");
+          select = sc.next();
+          UserSelectAndConnect.userSelect(select);
     }
   }
 }
