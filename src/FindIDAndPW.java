5f0f481c0fc521c499ddabb6eeee8cfeb241d89a
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2022-12-21 18:43:26 +0900

    FindIDAndPW : UserData의 형식에 맞춰 변경

--- src/FindIDAndPW.java
+++ src/FindIDAndPW.java
@@ -1,49 +1,34 @@
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class FindIDAndPW {
-  static Scanner sc = new Scanner(System.in);
-  static CreateID createID = new CreateID();
+  static UserData userData = new UserData();
 
   public static void findIDAndPW() {
-    System.out.println("아이디 찾기를 원하시면 1, 비밀번호 찾기를 원하시면 2를 입력해 주세요");
-    String idAndPWFind = sc.next();
-    IDAndPWexception(idAndPWFind);
-    if (idAndPWFind.equals("1")) {
-      //회원가입 시 개인정보 구문 추가
-      findID();
-    }
-    if (idAndPWFind.equals("2")) {
-      findPW();
-    }
-
+    Scanner sc = new Scanner(System.in);
+    System.out.println("이름을 입력해 주세요");
+    String name = sc.next();
+    System.out.println("주민번호를 입력해 주세요");
+    String RRN = sc.next();
+    IDAndPWCompare(name, RRN);
   }
+  public static void IDAndPWCompare(String name, String RRN){
+    List<List<String>> userInfos = new ArrayList<>(userData.getUserDatas());
+    for (int i = 0; i < userInfos.size(); i++) {
+      if (userInfos.get(i).get(0).contains(name)) {
+        if (userInfos.get(i).get(1).contains(RRN)) {
+          System.out.println("아이디는 " + userInfos.get(i).get(2) + ", 비밀번호는 " + userInfos.get(i).get(3) + "입니다.\n");
+          break;
+        }
+        if (i == userInfos.size() - 1 && !userInfos.get(i).get(1).contains(RRN)) {
+          System.out.println("일치하는 정보가 없습니다"); // 이름은 있으나 주민번호가 존재하지 않을 시
+        }
+      }
+      if (i == userInfos.size() - 1 && !userInfos.get(i).get(0).contains(name)) {
+        System.out.println("등록되지 않은 정보입니다"); // 이름이 존재하지 않을 시
+      }
+    }
 
-  public static void IDAndPWexception(String IDAndPW) {
-    if (IDAndPW.length() != 1) {
-      throw new IllegalArgumentException("잘못된 값 입력");
-    }
-    if (!Character.isDigit(IDAndPW.charAt(0))) {
-      throw new IllegalArgumentException("숫자만 입력 가능");
-    }
-    if (Integer.parseInt(IDAndPW) != 1 && Integer.parseInt(IDAndPW) != 2) {
-      throw new IllegalArgumentException("ERROR : 1, 2만 입력 가능");
-    }
-  }
-
-  public static void findID() {
-
-  }
-
-  public static void findPW() {
-    System.out.println("아이디를 입력해 주세요");
-    String ID = sc.next();
-    HashMap<String, String> findID = new HashMap<>(createID.getIDAndPW());
-    if (findID.containsKey(ID)) {
-      System.out.println("패스워드는 " + findID.get(ID) + " 입니다");
-    }
-    else{
-      System.out.println("아이디를 한번 더 확인해 주세요");
-    }
   }
 }
